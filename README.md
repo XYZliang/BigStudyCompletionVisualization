bf0d4c948d00e1dfd3f8e0e4c44ceb9dff587fe6
张亮 <59312540+XYZliang@users.noreply.github.com>
2021-12-18 17:05:30 +0800

    V1.0 登录实现

---
+++ README.md
@@ -0,0 +1,45 @@
+# BigStydyCompletionVisualization
+ 江西高校青年大学习未完成名单一键式导出爬虫脚本（正在制作），适用于江西省云瓣大学习系统，江西共青团公众号
+
+## 如何使用？
+
+1. 将仓库获取/下载到本地
+
+2. 修改默认配置文件BigStudyConfigTemplate.cfg中的账号密码为自己系统的账号密码
+
+3. 将BigStudyConfigTemplate.cfg修改为BigStudyConfig.cfg
+
+4. pip install 以下依赖
+
+   ```shell
+   pip install configparser
+   pip install orjson
+   pip install faker
+   pip install orjson
+   pip install --upgrade "pip>=20.3" # 绝大多数linux_x_y的支持, 以及苹果M1芯片的适配
+   ```
+
+5. 运行main.py
+
+   ```
+   python3 main.py
+   ```
+
+   
+
+## 要干什么？
+
+- [x] 爬虫登录
+- [ ] 爬虫爬取
+- [ ] 可视化输出
+- [ ] 输出至网页
+
+
+
+## 测试环境
+
+python3.9
+
+macOS 12.2 beta 1
+
+PyCharm 2021.2.3
---
+++ main.py
@@ -0,0 +1,150 @@
+# -* - coding: UTF-8 -* -
+import configparser
+import os
+import time
+import urllib
+
+import orjson
+import requests
+from faker import Factory
+
+# 登录的账号密码
+
+account = ""
+password = ""
+
+# 全局变量
+# token值
+Token = ""
+
+# 其他工具变量
+headers = {
+    "User-Agent": ""
+}
+proxies = {"http": None, "https": None}
+
+
+# 效验token
+def checkToken():
+    data = readDataFromFile("loginToken", False)
+    if (data == False):
+        print("token不存在，重新获取")
+        login()
+    else:
+        print(data)
+        data = orjson.loads(data)
+        Token = data["token"]
+        headers["UA"] = data["UA"]
+        url = "https://jxtw.h5yunban.cn/jxtw-qndxx/cgi-bin/branch-api/course/list"
+        values = {}
+        values['pageSize'] = '1000'
+        values['pageNum'] = '1'
+        values['accessToken'] = Token
+        status = sendGet(url, values)
+        if (status == False):
+            login()
+
+
+# 登录爬虫函数
+def login():
+    fc = Factory.create()
+    UA = fc.user_agent()
+    headers["UA"] = UA
+    # 登录API
+    url = "https://jxtw.h5yunban.cn/jxtw-qndxx/cgi-bin/login"
+    # application/json参数
+    values = {"account": account, "password": password}
+    Token = sendPostJson(url, values)['accessToken']
+    loginData = {"token": Token, "time": time.time(), "UA": UA}
+    loginData = orjson.dumps(loginData)
+    saveDataToFile("loginToken", loginData, True)
+
+
+# POST请求函数
+def sendPostJson(url, values):
+    # json.dump将python对象编码成json字符串
+    values_json = orjson.dumps(values)
+    # requests库提交数据进行post请求
+    req = requests.post(url, data=values_json, headers=headers, proxies=proxies)
+    # 使用json.dumps()时需要对象相应的类型是json可序列化的
+    change = req.json()
+    # 转换为JSON对象
+    json_req = orjson.dumps(change)
+    json_req = orjson.loads(json_req)
+    result = json_req.get("result")
+    if (json_req.get("status") != 200):
+        print("请求错误：" + result)
+        exit()
+    return result
+
+
+# GET请求函数
+def sendGet(url, values):
+    # 对请求数据进行编码
+    data = urllib.parse.urlencode(values).encode('utf-8')
+    # 若为post请求以下方式会报错TypeError: POST data should be bytes, an iterable of bytes, or a file object. It cannot be of type str.
+    # Post的数据必须是bytes或者iterable of bytes,不能是str,如果是str需要进行encode()编码
+    data = urllib.parse.urlencode(values)
+    # 将数据与url进行拼接
+    req = url + '?' + data
+    # 打开请求，获取对象
+    response = requests.get(req, headers=headers, proxies=proxies)
+    # 读取服务器返回的数据,对HTTPResponse类型数据进行读取操作
+    the_page = response.text
+    print(the_page)
+    json_req = orjson.loads(the_page)
+    result = json_req.get("result")
+    if (json_req.get("status") != 200):
+        print("请求错误：" + result)
+        exit()
+    return result
+
+
+def saveDataToFile(fileName, data, isByte):
+    if (isByte):
+        file = open(fileName + ".data", "wb")
+    else:
+        file = open(fileName + ".data", "w")
+    file.write(data)
+    file.close()
+
+
+def readDataFromFile(fileName, isByte):
+    try:
+        if (isByte):
+            file = open(fileName + ".data", "rb")
+        else:
+            file = open(fileName + ".data", "r")
+        data = file.read()
+        file.close()
+    except:
+        return False
+    return data
+
+
+def readConfig():
+    # 判断配置文件是否存在
+    if (os.path.exists("BigStudyConfig.cfg") or os.path.exists("BigStudyConfigTemplate.cfg")):
+        if (os.path.exists("BigStudyConfig.cfg")):
+            print("获取到配置文件")
+        else:
+            print("获取不到配置文件，请修改配置模板文件BigStudyConfigTemplate.cfg重命名为BigStudyConfig.cfg")
+            exit()
+    else:
+        print("配置文件和配置模板文件均已丢失，请从github从新获取")
+        print("")
+        exit()
+    # 生成config对象
+    conf = configparser.ConfigParser()
+    conf.sections()
+    # 用config对象读取配置文件
+    conf.read("BigStudyConfig.cfg")
+    # 以列表形式返回所有的section
+    sections = conf.sections()
+    account = conf["DEFAULT"]["Account"]
+    password = conf["DEFAULT"]["Password"]
+
+
+if __name__ == '__main__':
+    readConfig()
+    checkToken()
